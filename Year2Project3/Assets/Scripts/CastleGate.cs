f61296c35397a33ae52a05f591c8f180594362fc
Dominik Beens <dominik.beens@hotmail.nl>
2018-02-12 17:40:07 +0100

    added a working gate

---
+++ Year2Project3/Assets/Scripts/CastleGate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CastleGate : MonoBehaviour 
+{
+
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("GateOpen"))
+        {
+            CloseGate();
+        }
+        else
+        {
+            OpenGate();
+        }
+    }
+
+    private void OpenGate()
+    {
+        float currentAnimationProgress = anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+
+        if (currentAnimationProgress < 1)
+        {
+            anim.Play("GateOpen", 0, (1 - currentAnimationProgress));
+        }
+        else
+        {
+            anim.Play("GateOpen");
+        }
+    }
+
+    private void CloseGate()
+    {
+        float currentAnimationProgress = anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+
+        if (currentAnimationProgress < 1)
+        {
+            anim.Play("GateClose", 0, (1- currentAnimationProgress));
+        }
+        else
+        {
+            anim.Play("GateClose");
+        }
+    }
+}
