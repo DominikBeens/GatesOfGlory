6c1ef4cdba7ad5509c1fd88a5ca153668295cfa0
Dominik Beens <dominik.beens@hotmail.nl>
2018-02-13 16:28:24 +0100

    gate health bar

--- Year2Project3/Assets/Scripts/Wave And BattleField/Damagebles/Castle Deffense Point/CastleDeffensePoint.cs
+++ Year2Project3/Assets/Scripts/Wave And BattleField/Damagebles/Castle Deffense Point/CastleDeffensePoint.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CastleDeffensePoint : Damagebles {
     public bool gateOpen;
     public List<Enemy> attackingMe = new List<Enemy>();
     public CastleGate myGate;
+    public Image healthbarFill;
 
     public override void TakeDamage(float damage){
         myStats.health.currentValue -= damage;
+        healthbarFill.fillAmount = myStats.health.currentValue / myStats.health.baseValue;
         if(myStats.health.currentValue < 0){
             myGate.OpenGate();
             gateOpen = true;
