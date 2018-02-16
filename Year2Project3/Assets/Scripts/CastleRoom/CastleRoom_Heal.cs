c3aed06b2e84705c349aabeda8a717717942d2b3
Dominik Beens <dominik.beens@hotmail.nl>
2018-02-16 15:57:51 +0100

    added healing room

--- Year2Project3/Assets/Scripts/CastleRoom/CastleRoom_Heal.cs
+++ Year2Project3/Assets/Scripts/CastleRoom/CastleRoom_Heal.cs
@@ -8,6 +8,7 @@ public class CastleRoom_Heal : CastleRoom
     [Header("Properties")]
     public Stat useCooldown;
     public Stat healAmount;
+    public GameObject healParticle;
 
     public override void SetupUI()
     {
@@ -21,6 +22,9 @@ public class CastleRoom_Heal : CastleRoom
         for (int i = 0; i < WaveManager.instance.alliesInScene.Count; i++)
         {
             WaveManager.instance.alliesInScene[i].TakeDamage(-healAmount.currentValue);
+
+            Vector3 particleSpawn = new Vector3(WaveManager.instance.alliesInScene[i].transform.position.x, WaveManager.instance.alliesInScene[i].transform.position.y + 1.5f);
+            Instantiate(healParticle, particleSpawn, Quaternion.identity);
         }
     }
 }
