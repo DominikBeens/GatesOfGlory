a2c114501f3b83508bc8e5fafef872fc9955c9ec
HanHakkenbergDeltion <han.hakkenberg@gmail.com>
2018-02-18 00:16:28 +0100

    AI Inproved

--- Year2Project3/Assets/Scripts/Wave And BattleField/BattleManager.cs
+++ Year2Project3/Assets/Scripts/Wave And BattleField/BattleManager.cs
@@ -2,108 +2,191 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleManager : MonoBehaviour{
+public class BattleManager : MonoBehaviour
+{
     public float soldierOffset;
 
-    public Transform gateRightInside, gateLeftInside;
-    public Transform gateRight,gateLeft;
+    public List<DefensePoint> newDeffensePoints = new List<DefensePoint>();
+    List<DefensePoint> RightDeffensePoints = new List<DefensePoint>();
+    List<DefensePoint> LeftDeffensePoints = new List<DefensePoint>();
     public Transform throne;
-
-    public Transform gateRightAlly, GateLeftAlly;
+    public float min, max; //Remove
+    public float maxEnemySearch, minEnemySearch;
     public static BattleManager instance;
     public List<GameObject> freeEnemys = new List<GameObject>();
 
     void Awake(){
         instance = this;
+        foreach (DefensePoint point in newDeffensePoints){
+            if (point.gate != null){
+                if (point.gate.position.x > 0){
+                    if (RightDeffensePoints.Count == 0){
+                        RightDeffensePoints.Add(point);
+                    }
+                    else{
+                        for (int i = 0; i < RightDeffensePoints.Count; i++){
+                            if (RightDeffensePoints[i].gate.position.x < point.gate.position.x){
+                                RightDeffensePoints.Insert(i, point);
+                                break;
+                            }
+                            else if (i == 0){
+                                RightDeffensePoints.Add(point);
+                            }
+                        }
+                    }
+
+                }
+                else{
+                    if (LeftDeffensePoints.Count == 0){
+                        LeftDeffensePoints.Add(point);
+                    }
+                    else{
+                        for (int i = 0; i < LeftDeffensePoints.Count; i++){
+                            if (LeftDeffensePoints[i].gate.position.x > point.gate.position.x){
+                                LeftDeffensePoints.Insert(i, point);
+                                break;
+                            }
+                            else if (i == LeftDeffensePoints.Count - 1){
+                                LeftDeffensePoints.Add(point);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
     }
 
     public Transform EnemyGetTarget(float _myPosX){
         if (_myPosX >= 0){
-            if (gateRight.GetComponent<CastleDeffensePoint>().gateOpen == true){
-                return (throne);
+            for (int i = 0; i < RightDeffensePoints.Count; i++){
+                if (RightDeffensePoints[i].gate.position.x > _myPosX || !RightDeffensePoints[i].gate.GetComponent<Gate>().gateOpen && RightDeffensePoints[i].gate.position.x > _myPosX){
+                    return (RightDeffensePoints[i].gate.transform);
+                }
             }
-            else if(_myPosX > gateRight.position.x){
-                return (gateRight);
-            }
-            else{
-                return (throne);
-            }
+            return (throne);
         }
         else{
-            if (gateLeft.GetComponent<CastleDeffensePoint>().gateOpen == true){
-                return (throne);
+            for (int i = 0; i < LeftDeffensePoints.Count; i++){
+                if (LeftDeffensePoints[i].gate.position.x > _myPosX || !LeftDeffensePoints[i].gate.GetComponent<Gate>().gateOpen && LeftDeffensePoints[i].gate.position.x > _myPosX){
+                    return (LeftDeffensePoints[i].gate.transform);
+                }
             }
-            else if(_myPosX < gateLeft.position.x)
-            {
-                return (gateLeft);
-            }
-            else{
-                return (throne);
-            }
+            return (throne);
         }
     }
 
     //finding a path to a enemy is there is none go to closest reachable gate
     public Transform AllyGetTarget(float _myPosX, Allie _me){
-        if(_myPosX >= 0){
-            if(gateRight.GetComponent<CastleDeffensePoint>().gateOpen){
-                GameObject _currentTarget = GetClosest(1000,0, _me);
-                if(_currentTarget == null){
-                    return(gateRightAlly);
-                }
-                return (_currentTarget.transform);
-            }
-            else{
-                if(_myPosX < gateRight.transform.position.x){
-                    GameObject _currentTarget = GetClosest(gateRightInside.transform.position.x,0, _me);
-                    if(_currentTarget == null){
-                        return (gateRightInside);
+        Transform _bestGate = null;
+        float _min = minEnemySearch;
+        float _max = maxEnemySearch;
+
+        if (_myPosX >= 0){
+            for (int i = 0; i < RightDeffensePoints.Count; i++){
+                if (!RightDeffensePoints[i].gate.GetComponent<Gate>().gateOpen){
+                    if (_myPosX > RightDeffensePoints[i].gate.position.x){
+                        if (i == 0){
+                            _bestGate = RightDeffensePoints[i].frontGate;
+                            _min = RightDeffensePoints[i].frontGate.position.x;
+                        }
+                        else{
+                            _min = RightDeffensePoints[i].gate.position.x;
+                        }
+                        break;
                     }
-                    return (_currentTarget.transform);
+                    else{
+                        _bestGate = RightDeffensePoints[i].backGate;
+                        _max = RightDeffensePoints[i].backGate.position.x;
+                    }
                 }
                 else{
-                    GameObject _currentTarget = GetClosest(1000, gateRight.transform.position.x, _me);
-                    if(_currentTarget == null){
-                        return (gateRightAlly);
+                    if (i == 0){
+                        _max = maxEnemySearch;
+                        _bestGate = RightDeffensePoints[i].frontGate;
                     }
-                    return (_currentTarget.transform);
                 }
             }
+            if (_min < 0){
+                for (int i = 0; i < LeftDeffensePoints.Count; i++){
+                    if (!LeftDeffensePoints[i].gate.GetComponent<Gate>().gateOpen){
+                        _min = LeftDeffensePoints[i].backGate.position.x;
+                    }
+                    else{
+                        if (i - 1 > 0){
+                            _min = LeftDeffensePoints[i - 1].backGate.position.x;
+                        }
+                        else{
+                            _min = minEnemySearch;
+                        }
+                    }
+                }
+            }
+
         }
         else{
-            if (gateLeft.GetComponent<CastleDeffensePoint>().gateOpen){
-                GameObject _currentTarget = GetClosest(0, -1000,_me);
-                if (_currentTarget == null){
-                    return (GateLeftAlly);
-                }
-                return (_currentTarget.transform);
-            }
-            else{
-                if (_myPosX > gateLeft.transform.position.x){
-                    GameObject _currentTarget = GetClosest(0, gateLeftInside.transform.position.x, _me);
-                    if (_currentTarget == null){
-                        return (gateLeftInside);
+            for (int i = 0; i < LeftDeffensePoints.Count; i++){
+                if (!LeftDeffensePoints[i].gate.GetComponent<Gate>().gateOpen){
+                    if (_myPosX < LeftDeffensePoints[i].gate.position.x){
+                        if (i == 0){
+                            _bestGate = LeftDeffensePoints[i].frontGate;
+                            _max = LeftDeffensePoints[i].frontGate.position.x;
+                        }
+                        else{
+                            _max = LeftDeffensePoints[i].gate.position.x;
+                        }
+                        break;
                     }
-                    return (_currentTarget.transform);
+                    else{
+                        _bestGate = LeftDeffensePoints[i].backGate;
+                        _min = LeftDeffensePoints[i].backGate.position.x;
+                    }
                 }
                 else{
-                    GameObject _currentTarget = GetClosest(gateRight.transform.position.x ,- 1000, _me);
-                    if (_currentTarget == null){
-                        return (GateLeftAlly);
+                    if (i == 0){
+                        _min = minEnemySearch;
+                        _bestGate = LeftDeffensePoints[i].frontGate;
                     }
-                    return (_currentTarget.transform);
+                }
+            }
+            if (_max > 0){
+                for (int i = 0; i < RightDeffensePoints.Count; i++){
+                    if (!RightDeffensePoints[i].gate.GetComponent<Gate>().gateOpen){
+                        _max = RightDeffensePoints[i].backGate.position.x;
+                    }
+                    else{
+                        if (i - 1 > 0){
+                            _max = RightDeffensePoints[i - 1].backGate.position.x;
+                        }
+                        else{
+                            _max = maxEnemySearch;
+                        }
+                    }
                 }
             }
         }
+        if (freeEnemys.Count > 0){
+            GameObject _nextEnemt = GetClosest(_max, _min, _me);
+            if (_nextEnemt != null){
+                min = _min;
+                max = _max;
+                return (_nextEnemt.transform);
+            }
+        }
+        min = _min;
+        max = _max;
+        return (_bestGate);
     }
 
-    public GameObject GetClosest(float maxDistance, float minDistance, Allie _me){
+    GameObject GetClosest(float maxDistance, float minDistance, Allie _me)
+    {
         GameObject closest = null;
-        foreach(GameObject t in freeEnemys){
-            if(t == null){
+        foreach (GameObject t in freeEnemys){
+            if (t == null){
                 freeEnemys.Remove(t);
             }
-            if (t.transform.position.x > minDistance && t.transform.position.x < maxDistance){
+            if (t.transform.position.x < maxDistance && t.transform.position.x > minDistance){
                 if (closest == null){
                     closest = t;
                 }
@@ -112,9 +195,13 @@ public class BattleManager : MonoBehaviour{
                 }
             }
         }
-        if(closest != null){
-            closest.GetComponent<Enemy>().AddCounter(_me);
-        }
         return (closest);
+    }
+
+    [System.Serializable]
+    public struct DefensePoint{
+        public Transform frontGate;
+        public Transform backGate;
+        public Transform gate;
     }
 }
--- Year2Project3/Assets/Scripts/Wave And BattleField/Damagebles/Soldier/Ally/Knight/Knight.cs
+++ Year2Project3/Assets/Scripts/Wave And BattleField/Damagebles/Soldier/Ally/Knight/Knight.cs
@@ -2,33 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Knight : Allie{
-    void Start(){
-        targetTransform = BattleManager.instance.AllyGetTarget(transform.position.x,this);
+public class Knight : Allie
+{
+    void Start()
+    {
+        targetTransform = BattleManager.instance.AllyGetTarget(transform.position.x, this);
         agent.SetDestination(targetTransform.position);
     }
 
 
-    void OnTriggerEnter(Collider other){
-        if (targetTransform != null && targetTransform == other.transform){
+    void OnTriggerEnter(Collider other)
+    {
+        if (targetTransform != null && targetTransform == other.transform)
+        {
             targetTransform.gameObject.GetComponent<Enemy>().StartBattle(this);
             agent.isStopped = true;
             StartCoroutine(Attack());
         }
     }
 
-    void OnCollisionEnter(Collision collision){
-        if(collision.transform == targetTransform){
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform == targetTransform)
+        {
             agent.isStopped = true;
         }
     }
 
-    void OnCollisionStay(Collision collision){
-        if (collision.transform != targetTransform){
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform != targetTransform)
+        {
             GetNewTarget();
             agent.isStopped = false;
         }
-        else{
+        else
+        {
             GetNewTarget();
             agent.isStopped = true;
         }
