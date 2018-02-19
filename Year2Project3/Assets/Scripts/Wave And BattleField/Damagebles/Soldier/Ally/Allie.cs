95daf282b4aeddf0702c996f9f84dd337cab9595
HanHakkenbergDeltion <han.hakkenberg@gmail.com>
2018-02-19 17:54:20 +0100

    AI Changes And Gate Correction

--- Year2Project3/Assets/Scripts/Wave And BattleField/Damagebles/Soldier/Ally/Allie.cs
+++ Year2Project3/Assets/Scripts/Wave And BattleField/Damagebles/Soldier/Ally/Allie.cs
@@ -2,40 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Allie : Soldier{
+public class Allie : Soldier
+{
 
-    public override void Update(){
+    void Start()
+    {
+        targetTransform = BattleManager.instance.AllyGetTarget(transform.position.x, this);
+        agent.SetDestination(targetTransform.position);
+    }
+
+    public override void Update()
+    {
         base.Update();
-        if (targetTransform == null){
-            targetTransform = BattleManager.instance.AllyGetTarget(transform.position.x,this);
+        GetNewTarget();
+    }
+
+    public void GetNewTarget()
+    {
+        if (targetTransform != null)
+        {
+            Transform newTarget = BattleManager.instance.AllyGetTarget(transform.position.x, this); //
+            if (newTarget != targetTransform)
+            {
+                if (targetTransform.tag == "Enemy")
+                {
+                    if (newTarget.tag != "Enemy")
+                    {
+                        targetTransform.GetComponent<Enemy>().RemoveCounter(this);
+                        targetTransform = newTarget;
+                        agent.SetDestination(targetTransform.position);
+                        agent.isStopped = false;
+                    }
+                    else if (agent.isStopped == false)
+                    {
+                        agent.SetDestination(targetTransform.position);
+                    }
+                }
+                else
+                {
+                    if (newTarget.tag == "Enemy")
+                    {
+                        newTarget.GetComponent<Enemy>().AddCounter(this);
+                    }
+                    targetTransform = newTarget;
+                    agent.SetDestination(targetTransform.position);
+                    agent.isStopped = false;
+                }
+            }
+            else if (targetTransform.tag == "Enemy" && agent.isStopped == false)
+            {
+                agent.SetDestination(targetTransform.position);
+            }
+        }
+        else
+        {
+            targetTransform = BattleManager.instance.AllyGetTarget(transform.position.x, this); //
             agent.SetDestination(targetTransform.position);
             agent.isStopped = false;
         }
-        else if(agent.isStopped == false){
-            agent.SetDestination(targetTransform.position);
-        }
     }
 
-    public void GetNewTarget(){
-        if(targetTransform != null && targetTransform.tag != "Enemy"){
-            targetTransform = BattleManager.instance.AllyGetTarget(transform.position.x, this);
-            agent.SetDestination(targetTransform.position);
-            agent.isStopped = false;
-        }
-    }
-
-    public override void TakeDamage(float damage){
+    public override void TakeDamage(float damage)
+    {
         myStats.health.currentValue -= damage;
-        if (myStats.health.currentValue <= 0){
+        if (myStats.health.currentValue <= 0)
+        {
+            if(targetTransform != null)
+            {
             targetTransform.GetComponent<Enemy>().RemoveCounter(this);
+            }
             Destroy(gameObject);
         }
     }
 
-    public IEnumerator Attack(){
+    public IEnumerator Attack()
+    {
         Transform _attackingCurrently = targetTransform;
         yield return new WaitForSeconds(attackCooldown);
-        if (targetTransform == _attackingCurrently){
+        if (targetTransform != null && targetTransform.tag == "Enemy" && targetTransform == _attackingCurrently)
+        {
             targetTransform.GetComponent<Enemy>().TakeDamage(myStats.damage.currentValue);
             //death Animation
             StartCoroutine(Attack());
