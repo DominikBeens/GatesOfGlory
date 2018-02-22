56497f0cc2c6340adb431627b9500d66232b4822
HanHakkenbergDeltion <han.hakkenberg@gmail.com>
2018-02-22 22:23:51 +0100

    throne Gold Remove

--- Year2Project3/Assets/Scripts/Wave And BattleField/Damagebles/Castle Deffense Point/Throne/Throne.cs
+++ Year2Project3/Assets/Scripts/Wave And BattleField/Damagebles/Castle Deffense Point/Throne/Throne.cs
@@ -3,6 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Throne : CastleDeffensePoint{
+public class Throne : CastleDeffensePoint
+{
+    public override void TakeDamage(float damage)
+    {
+        if (ResourceManager.gold >= damage)
+        {
+            ResourceManager.gold -= Mathf.RoundToInt(damage);
+            ResourceManager.instance.RemoveGold(Mathf.RoundToInt(damage));
+        }
+        else if(ResourceManager.gold > 0)
+        {
+            damage = Mathf.Abs(ResourceManager.gold - Mathf.RoundToInt(damage));
+            ResourceManager.instance.RemoveGold(ResourceManager.gold);
+            myStats.health.currentValue -= damage;
+        }
+        else
+        {
+            myStats.health.currentValue -= damage;
+        }
 
+        if (healthbarFill != null)
+        {
+            healthbarFill.fillAmount = (myStats.health.currentValue / myStats.health.baseValue);
+        }
+    }
 }
