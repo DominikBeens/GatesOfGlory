f6229bf31b3b59f468583401653e5770c027fbe8
Dominik Beens <dominik.beens@hotmail.nl>
2018-03-20 21:18:06 +0100

    new very much wanted feature: boopables!

---
+++ Year2Project3/Assets/Scripts/Components/BoopOnClick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof (Animator), typeof(SphereCollider))]
+public class BoopOnClick : MonoBehaviour 
+{
+
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnMouseDown()
+    {
+        try
+        {
+            anim.Play("Boop");
+        }
+        catch (System.Exception)
+        {
+            throw;
+        }
+    }
+}
