cf65bfbea5d83f0a95442c9faae3d1fb0033f12b
Dominik Beens <dominik.beens@hotmail.nl>
2018-03-23 16:45:53 +0100

    implemented new textures, work on ne wshop

--- Year2Project3/Assets/Scripts/CastleRoom/Shop.cs
+++ Year2Project3/Assets/Scripts/CastleRoom/Shop.cs
@@ -15,6 +15,11 @@ public class Shop : MonoBehaviour
 
     public TextMeshProUGUI selectedShopItemText;
 
+    [Header("Cost")]
+    public int spikesCost;
+    public int oilCost;
+    public int archersCost;
+
     private void Awake()
     {
         uiPanel.SetActive(false);
@@ -57,5 +62,50 @@ public class Shop : MonoBehaviour
     public void ResetSelectedShopItemButton()
     {
         selectedShopItemText.text = "What would you like to buy?";
+    }
+
+    public void ShowShopItemCost(Animator a)
+    {
+        a.ResetTrigger("Close");
+        a.SetTrigger("Open");
+    }
+
+    public void HideShopItemCost(Animator a)
+    {
+        a.ResetTrigger("Open");
+        a.SetTrigger("Close");
+    }
+
+    public void BuySpikesButton(Animator a)
+    {
+        if (!ResourceManager.instance.HasEnoughGold(spikesCost))
+        {
+            return;
+        }
+
+        a.SetTrigger("Buy");
+        ResourceManager.instance.RemoveGold(spikesCost, true);
+    }
+
+    public void BuyOilButton(Animator a)
+    {
+        if (!ResourceManager.instance.HasEnoughGold(oilCost))
+        {
+            return;
+        }
+
+        a.SetTrigger("Buy");
+        ResourceManager.instance.RemoveGold(oilCost, true);
+    }
+
+    public void BuyArcherButton(Animator a)
+    {
+        if (!ResourceManager.instance.HasEnoughGold(archersCost))
+        {
+            return;
+        }
+
+        a.SetTrigger("Buy");
+        ResourceManager.instance.RemoveGold(archersCost, true);
     }
 }
