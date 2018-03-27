4bcb5e57167d4ad67ff3412c60d380b8ff212eb6
Dominik Beens <dominik.beens@hotmail.nl>
2018-03-27 14:39:05 +0200

    added repairable generator

---
+++ Year2Project3/Assets/Scripts/CastleRoom/Generator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Generator : MonoBehaviour 
+{
+
+    public Transform uiParent;
+    public GameObject uiPanel;
+    public GameObject uiOpenButton;
+
+    public Animator anim;
+    public Animator generatorAnim;
+
+    public GameObject brokenUI;
+    public GameObject repairedUI;
+
+    public int repairCost;
+    public TextMeshProUGUI repairCostText;
+
+    private void Awake()
+    {
+        uiPanel.SetActive(false);
+        repairCostText.text = repairCost.ToString();
+    }
+
+    private void Update()
+    {
+        if (uiPanel.activeInHierarchy)
+        {
+            uiParent.LookAt(Camera.main.transform);
+        }
+    }
+
+    public void OpenUIButton()
+    {
+        uiOpenButton.SetActive(false);
+        uiPanel.SetActive(true);
+    }
+
+    public void CloseUIButton()
+    {
+        StartCoroutine(CloseUI());
+    }
+
+    private IEnumerator CloseUI()
+    {
+        anim.SetTrigger("CloseUI");
+
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
+
+        uiPanel.SetActive(false);
+        uiOpenButton.SetActive(true);
+    }
+
+    public void RepairGeneratorButton()
+    {
+        if (!ResourceManager.instance.HasEnoughGold(repairCost))
+        {
+            return;
+        }
+
+        ResourceManager.instance.RemoveGold(repairCost, true);
+        generatorAnim.SetBool("Repaired", true);
+
+        uiPanel.SetActive(false);
+
+        brokenUI.SetActive(false);
+        repairedUI.SetActive(true);
+
+        uiPanel.SetActive(true);
+    }
+}
