3530f041d9728a4993bd8af11a9efe474b83d932
HanHakkenbergDeltion <han.hakkenberg@gmail.com>
2018-04-19 13:46:09 +0200

    bom

--- Year2Project3/Assets/Scripts/Wave And BattleField/Damagebles/Soldier/Enemy/EnemyTerrorist.cs
+++ Year2Project3/Assets/Scripts/Wave And BattleField/Damagebles/Soldier/Enemy/EnemyTerrorist.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyTerrorist : Enemy {
+    public bool k;
 
     public override void TakeDamage(float damage) {
         myStats.health.currentValue -= damage;
 
-        if(myStats.health.currentValue <= 0) {
+        if(myStats.health.currentValue <= 0 && !k) {
+            k = true;
             ObjectPooler.instance.AddToPool("Enemy Terrorist", gameObject);
             ResourceManager.instance.AddGold(ResourceManager.instance.terroristGoldReward);
         }
@@ -19,5 +21,9 @@ public class EnemyTerrorist : Enemy {
             ObjectPooler.instance.GrabFromPool("terrorist kaboom", new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Quaternion.identity);
             ObjectPooler.instance.AddToPool("Enemy Terrorist", gameObject);
         }
+    }
+
+    private void OnEnable() {
+        k = false;
     }
 }
